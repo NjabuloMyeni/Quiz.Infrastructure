545950df77e478b8d3818389e83fc86c872a154d
Simiso-02 <thembelani41268@gmail.com>
2024-04-21 22:20:25 +0200

    Fixing QAModel

--- Quiz.Infrastructure/server/Models/QAModel.cpp
+++ Quiz.Infrastructure/server/Models/QAModel.cpp
@@ -14,20 +14,3 @@ struct QAModel {
 
 };
 
-vector<QAModel>shuffle_and_stack(vector<QAModel>& questions, vector<int>& askedIndices) {
-
-	// Remove already asked questions
-	vector<QAModel> filteredQuestions;
-	for (int i = 0; i < questions.size(); ++i) {
-		if (find(askedIndices.begin(), askedIndices.end(), i) == askedIndices.end()) {
-			filteredQuestions.push_back(questions[i]);
-		}
-	}
-
-	// Stack the indices of the asked questions
-	for (int i = 0; i < filteredQuestions.size(); ++i) {
-		int index = find(questions.begin(), questions.end(), filteredQuestions[i]) - questions.begin();
-		askedIndices.push_back(index);
-	}
-	return filteredQuestions;
-};
